7486e1ca3f2ed1008ef25fe192f49641f52c8665
w0mTea <w0mt3a@gmail.com>
2018-09-13 09:49:55 +0800

    remove spaces between cjk characters in org mode

--- config/init-org.el
+++ config/init-org.el
@@ -16,7 +16,51 @@
   (define-key org-mode-map (kbd "M-C-S-p") 'org-shiftmetaup)
   (define-key org-mode-map (kbd "M-C-S-n") 'org-shiftmetadown)
   (define-key org-mode-map (kbd "M-C-S-b") 'org-shiftmetaleft)
-  (define-key org-mode-map (kbd "M-C-S-f") 'org-shiftmetaright))
+  (define-key org-mode-map (kbd "M-C-S-f") 'org-shiftmetaright)
+
+  ;; Set export backends
+  (setq org-export-backends '(ascii html latex md))
+
+  ;; Clear newlines when exporting cjk characters
+  (require 'ox)
+
+  (defun clear-single-linebreak-in-cjk-string (text)
+    "clear single line-break between cjk characters that is usually soft line-breaks"
+    (let ((regexp "[[:multibyte:]]")
+          (string text))
+      ;; delete space converted from newline
+      (setq string
+            (replace-regexp-in-string
+             (format "\\(%s\\) *\n *\\(%s\\)" regexp regexp)
+             "\\1\\2" string))
+      ;; delete space before bold
+      (setq string
+            (replace-regexp-in-string
+             (format "\\(%s\\) +\\(<\\)" regexp)
+             "\\1\\2" string))
+      ;; delete space after bold
+      (setq string
+            (replace-regexp-in-string
+             (format "\\(>\\) +\\(%s\\)" regexp)
+             "\\1\\2" string))
+      string))
+
+  (defun ox-html-clear-single-linebreak-for-cjk (string backend info)
+    (when (org-export-derived-backend-p backend 'html)
+      (clear-single-linebreak-in-cjk-string string)))
+
+  (defun ox-md-clear-single-linebreak-for-cjk (string backend info)
+    (when (org-export-derived-backend-p backend 'md)
+      (message string)
+      (clear-single-linebreak-in-cjk-string string)))
+
+  (add-to-list 'org-export-filter-plain-text-functions
+               'ox-html-clear-single-linebreak-for-cjk)
+  (add-to-list 'org-export-filter-plain-text-functions
+               'ox-md-clear-single-linebreak-for-cjk))
+
+(use-package ox-gfm
+  :after ox)
 
 
 (provide 'init-org)
--- init.el
+++ init.el
@@ -25,9 +25,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-backends (quote (ascii html latex md)))
  '(package-selected-packages
    (quote
-    (yasnippet helm-xref org cquery lsp-python company-lsp lsp-ui lsp-mode dante idris-mode popwin switch-window helm-projectile use-package undo-tree rainbow-delimiters magit-gitflow idea-darkula-theme helm company aggressive-indent))))
+    (ox-gfm yasnippet helm-xref org cquery lsp-python company-lsp lsp-ui lsp-mode dante idris-mode popwin switch-window helm-projectile use-package undo-tree rainbow-delimiters magit-gitflow idea-darkula-theme helm company aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
